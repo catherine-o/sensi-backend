0b1f02dd0b6f13e3dfc2ec133c7bdcdad4bf0b6b
catherine-o <catherine.ohara@outlook.com>
2019-10-19 19:12:18 -0600

    reverts back to previous cors

--- api/users.js
+++ api/users.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const bcrypt = require('bcrypt')
 const saltRounds = 15
 
+
 var AYLIENTextAPI = require('aylien_textapi');
 var textapi = new AYLIENTextAPI({
   application_id: process.env.AYLIEN_ID,
